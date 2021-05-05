106f020e36c42f715a98fe7adb31ad909393f1de
Gokul Bijoy <81373116+carlmax-here@users.noreply.github.com>
2021-05-05 16:43:25 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -7,11 +7,11 @@ from pyrogram.errors import FloodWait
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
-api_id = ""   # Get it from my.telegram.org
-api_hash = ""   # Get it from my.telegram.org
-feed_url = ""   # RSS Feed URL of the site.
-bot_token = ""   # Get it by creating a bot on https://t.me/botfather
-log_channel = ""   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
+api_id = "3548465"   # Get it from my.telegram.org
+api_hash = "57930b1b0a2635aa265441697dafb778"   # Get it from my.telegram.org
+feed_url = "https://www.1tamilmv.art/uploads/css_built_15/341e4a57816af3ba440d891ca87450ff_framework.css.c43703c706f76455fc88fac593248a30.css?v=4e52a1ea3c"   # RSS Feed URL of the site.
+bot_token = "1763064781:AAEpUFuk2B9zLIuGgIBUnFV-YEYSEIMBapc"   # Get it by creating a bot on https://t.me/botfather
+log_channel = " -1001418928930"   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
 check_interval = 5   # Check Interval in seconds.  
 max_instances = 5   # Max parallel instance to be used.
 if os.environ.get("ENV"):   # Add a ENV in Environment Variables if you wanna configure the bot via env vars.
@@ -24,7 +24,7 @@ if os.environ.get("ENV"):   # Add a ENV in Environment Variables if you wanna co
   max_instances = int(os.environ.get("MAX_INSTANCES", 5))
 
 if db.get_link(feed_url) == None:
-  db.update_link(feed_url, "*")
+  db.update_link(feed_url, "mongodb+srv://carlmax:carlmax@cluster0.p3wiv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
